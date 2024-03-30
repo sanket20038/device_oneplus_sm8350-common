3b8a212e72ed89d573d6eaec58733b9f47d3d135
Sanket Patil <89565851+sanket20038@users.noreply.github.com>
2024-03-30 14:46:09 +0530

    Update DolbySettingsFragment.java for dolby qpr2

--- DolbyManager/src/co/aospa/settings/dolby/DolbySettingsFragment.java
+++ DolbyManager/src/co/aospa/settings/dolby/DolbySettingsFragment.java
@@ -39,7 +39,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
 
 public class DolbySettingsFragment extends PreferenceFragment implements
         OnPreferenceChangeListener, OnCheckedChangeListener {
